b7d3fe883804a67367a44bf40379a7f038257089
Alberto González <alberto.gonzalez@distillery.com>
2022-07-15 10:36:27 -0500

    The initial logic for the cache set up was implemented.

--- services/cache.js
+++ services/cache.js
@@ -7,12 +7,24 @@ const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 const exect = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
+mongoose.Query.prototype.exec = async function () {
   console.log("I'm about to run a query");
   // console.log(this.getQuery());
   // console.log(this.mongooseCollection.name);
-  const key = Object.assign({}, this.getQuery(), {
+  const key =  JSON.stringify(Object.assign({}, this.getQuery(), {
     collection: this.mongooseCollection.name,
-  });
-  return exect.apply(this, arguments);
+  }));
+
+  //see if we have a value for 'key' in redis
+  const cacheValue = await client.get(key)
+
+  //If we do, return that
+  if(cacheValue){
+    console.log("🚀",cacheValue);
+  }
+
+  //Otherwise, issue the query and store the 
+  //result in redis
+  const result = await exect.apply(this, arguments);
+  console.log("💥",result);
 };
