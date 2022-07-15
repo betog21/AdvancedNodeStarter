3c12b929bfb82761e897816e1d0f094ae47dcac4
Alberto González <alberto.gonzalez@distillery.com>
2022-07-15 10:12:25 -0500

    The key was generated on the user that executes the query and the collection that has been exectued.

--- services/cache.js
+++ services/cache.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const exect = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function (){
-    console.log("I'm about to run a query");
-    // console.log(this.getQuery());
-    // console.log(this.mongooseCollection.name);
-    return exect.apply(this, arguments)
-}
+mongoose.Query.prototype.exec = function () {
+  console.log("I'm about to run a query");
+  // console.log(this.getQuery());
+  // console.log(this.mongooseCollection.name);
+  const key = Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name,
+  });
+  return exect.apply(this, arguments);
+};
