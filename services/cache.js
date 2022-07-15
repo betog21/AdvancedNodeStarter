e1152cc33c7d8d56bc5f50ccfe83a62286ce0488
Alberto González <alberto.gonzalez@distillery.com>
2022-07-15 10:08:06 -0500

    New logs were added before the execution of the queries.

--- services/cache.js
+++ services/cache.js
@@ -4,5 +4,7 @@ const exect = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function (){
     console.log("I'm about to run a query");
+    // console.log(this.getQuery());
+    // console.log(this.mongooseCollection.name);
     return exect.apply(this, arguments)
 }
