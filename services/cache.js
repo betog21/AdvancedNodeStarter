2d02dc29997bd3d71101dff34ba80dfee37e16c5
Alberto González <alberto.gonzalez@distillery.com>
2022-07-15 11:25:59 -0500

    A new cache functionality was implemented.

--- services/cache.js
+++ services/cache.js
@@ -5,10 +5,17 @@ const util = require("util");
 const redisUrl = "redis://127.0.0.1:6379";
 const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
-const exect = mongoose.Query.prototype.exec;
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.cache = function () {
+  this.useCache = true;
+  return this;
+};
 
 mongoose.Query.prototype.exec = async function () {
-  console.log("I'm about to run a query");
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
   // console.log(this.getQuery());
   // console.log(this.mongooseCollection.name);
   const key = JSON.stringify(
@@ -30,7 +37,7 @@ mongoose.Query.prototype.exec = async function () {
 
   //Otherwise, issue the query and store the
   //result in redis
-  const result = await exect.apply(this, arguments);
+  const result = await exec.apply(this, arguments);
   client.set(key, JSON.stringify(result));
   return result;
 };
