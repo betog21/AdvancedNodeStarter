b77a88639644b66ea79c99e288e0f7c5a6349873
Alberto González <alberto.gonzalez@distillery.com>
2022-07-15 09:44:59 -0500

    The cache file was used in the index file and the blogRoutes was restored to its initial functionality.

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,50 +1,30 @@
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
 
-const Blog = mongoose.model('Blog');
+const Blog = mongoose.model("Blog");
 
-module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+module.exports = (app) => {
+  app.get("/api/blogs/:id", requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
-      _id: req.params.id
+      _id: req.params.id,
     });
 
     res.send(blog);
   });
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const redis = require('redis')
-    const redisUrl = 'redis://127.0.0.1:6379'
-    const client = redis.createClient(redisUrl)
-    const util = require('util')
-    client.get = util.promisify(client.get)
-
-    //Do we have any cached data in redis related to this query
-    const cacheBlogs = await client.get(req.user.id)
-
-    //If yes, then respond to the request right away and return
-    if(cacheBlogs){
-      console.log("Serving from cache.");
-      return res.send(JSON.parse(cacheBlogs))
-    }
-
-    //If no, we need to respond to request and update our cache 
-    //to store the data
+  app.get("/api/blogs", requireLogin, async (req, res) => {
     const blogs = await Blog.find({ _user: req.user.id });
-
-    console.log("Serving from mongo db");
     res.send(blogs);
-    client.set(req.user.id, JSON.stringify(blogs))
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post("/api/blogs", requireLogin, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
       title,
       content,
-      _user: req.user.id
+      _user: req.user.id,
     });
 
     try {
